7b2da4e4381f419c50c9a9221a904b9db628c9ce
Yuichi Murata <mrk21info+github@gmail.com>
2015-02-15 18:16:20 +0900

    Changed an indent width of the ts files.

--- typescript_fluxxor/src/index.ts
+++ typescript_fluxxor/src/index.ts
@@ -9,155 +9,155 @@ import FluxxorStore = require('fluxxor/lib/store');
 
 // Store
 module ListStore {
-  export type ValueType = string;
-  
-  export class State {
-    list = Array<ValueType>();
-  }
-  
-  export module Action {
-    export function add(value: ValueType) {
-      this.dispatch("list:add", value);
+    export type ValueType = string;
+    
+    export class State {
+        list = Array<ValueType>();
     }
     
-    export function clear() {
-      this.dispatch("list:clear");
-    }
-  }
-  
-  export class Store extends FluxxorStore {
-    state = new State;
-    
-    constructor() {
-      super();
-      this.bindActions(
-        "list:add", this.onAdd.bind(this),
-        "list:clear", this.onClear.bind(this)
-      );
+    export module Action {
+        export function add(value: ValueType) {
+            this.dispatch("list:add", value);
+        }
+        
+        export function clear() {
+            this.dispatch("list:clear");
+        }
     }
     
-    onAdd(value: ValueType) {
-      this.state.list.push(value);
-      this.emit("change");
+    export class Store extends FluxxorStore {
+        state = new State;
+        
+        constructor() {
+            super();
+            this.bindActions(
+                "list:add", this.onAdd.bind(this),
+                "list:clear", this.onClear.bind(this)
+            );
+        }
+        
+        onAdd(value: ValueType) {
+            this.state.list.push(value);
+            this.emit("change");
+        }
+        
+        onClear() {
+            this.state.list = [];
+            this.emit("change");
+        }
     }
-    
-    onClear() {
-      this.state.list = [];
-      this.emit("change");
-    }
-  }
 }
 
 // Component
 module ListComponent {
-  class State {
-    list: ListStore.State;
-    
-    constructor(flux: Fluxxor.Flux) {
-      this.list = flux.store<ListStore.Store>('list').state;
-    }
-  }
-  
-  class Spec extends TypedReact.Component<any, State> implements Fluxxor.Mixin.Flux, Fluxxor.Mixin.StoreWatch<State> {
-    getFlux: () => Fluxxor.Flux;
-    
-    getStateFromFlux() {
-      return new State(this.getFlux());
+    class State {
+        list: ListStore.State;
+        
+        constructor(flux: Fluxxor.Flux) {
+            this.list = flux.store<ListStore.Store>('list').state;
+        }
     }
     
-    render() {
-      return React.jsx(`
-        <div>
-          <button onClick={this.onAdd}>add</button>
-          <button onClick={this.onClear}>clear</button>
-          <ul>{this.state.list.list.map((v) =>
-            <li>{v}</li>
-          )}</ul>
-        </div>
-      `);
+    class Spec extends TypedReact.Component<any, State> implements Fluxxor.Mixin.Flux, Fluxxor.Mixin.StoreWatch<State> {
+        getFlux: () => Fluxxor.Flux;
+        
+        getStateFromFlux() {
+            return new State(this.getFlux());
+        }
+        
+        render() {
+            return React.jsx(`
+                <div>
+                    <button onClick={this.onAdd}>add</button>
+                    <button onClick={this.onClear}>clear</button>
+                    <ul>{this.state.list.list.map((v) =>
+                        <li>{v}</li>
+                    )}</ul>
+                </div>
+            `);
+        }
+        
+        onAdd() {
+            this.getFlux().actions.list.add(`${this.state.list.list.length} clicked`);
+        }
+        
+        onClear() {
+            this.getFlux().actions.list.clear();
+        }
     }
     
-    onAdd() {
-      this.getFlux().actions.list.add(`${this.state.list.list.length} clicked`);
-    }
-    
-    onClear() {
-      this.getFlux().actions.list.clear();
-    }
-  }
-  
-  export var Component = TypedReact.createClass(Spec, [
-    Fluxxor.FluxMixin(React),
-    Fluxxor.StoreWatchMixin<State>('list')
-  ]);
+    export var Component = TypedReact.createClass(Spec, [
+        Fluxxor.FluxMixin(React),
+        Fluxxor.StoreWatchMixin<State>('list')
+    ]);
 }
 
 module InfoComponent {
-  class Spec extends TypedReact.Component<any,any> {
-    render() {
-      return React.jsx(`<p>info</p>`);
+    class Spec extends TypedReact.Component<any,any> {
+        render() {
+            return React.jsx(`<p>info</p>`);
+        }
     }
-  }
-  
-  export var Component = TypedReact.createClass(Spec);
+    
+    export var Component = TypedReact.createClass(Spec);
 }
 
 module AppComponent {
-  class Spec extends TypedReact.Component<any,any> {
-    render() {
-      var Link = Router.Link;
-      var RouteHandler = Router.RouteHandler;
-      
-      return React.jsx(`
-        <div>
-          <h1><Link to="home">www</Link></h1>
-          <ul>
-            <li><Link to="info">info</Link></li>
-          </ul>
-          <RouteHandler {...this.props} />
-        </div>
-      `);
+    class Spec extends TypedReact.Component<any,any> {
+        render() {
+            var Link = Router.Link;
+            var RouteHandler = Router.RouteHandler;
+            
+            return React.jsx(`
+                <div>
+                    <h1><Link to="home">www</Link></h1>
+                    <ul>
+                        <li><Link to="info">info</Link></li>
+                    </ul>
+                    <RouteHandler {...this.props} />
+                </div>
+            `);
+        }
     }
-  }
-  
-  export var Component = TypedReact.createClass(Spec);
+    
+    export var Component = TypedReact.createClass(Spec);
 }
 
 // Flux
 module Flux {
-  var App = AppComponent.Component;
-  var Info = InfoComponent.Component;
-  var List = ListComponent.Component;
-  
-  module Stores {
-    export var list = new ListStore.Store();
-  }
-  
-  module Actions {
-    export var list = ListStore.Action;
-  };
-  
-  module Routes {
-    var Route = Router.Route;
-    var DefaultRoute = Router.DefaultRoute;
+    var App = AppComponent.Component;
+    var Info = InfoComponent.Component;
+    var List = ListComponent.Component;
     
-    export var Component = React.jsx(`
-      <Route handler={App} name="home" path="/">
-        <Route name="info" handler={Info} />
-        <DefaultRoute handler={List} />
-      </Route>
-    `);
-  }
-  
-  var flux = new Fluxxor.Flux(Stores, Actions);
-  
-  flux.on("dispatch", (type: string, payload: any) => {
-    if (console && console.log) {
-      console.log("[Dispatch]", type, payload);
+    module Stores {
+        export var list = new ListStore.Store();
     }
-  });
-  
-  Router.run(Routes.Component, (RouteHandler: any, state: any) => {
-    React.render(React.jsx(`<RouteHandler flux={flux} />`), document.getElementById("app"));
-  });
+    
+    module Actions {
+        export var list = ListStore.Action;
+    };
+    
+    module Routes {
+        var Route = Router.Route;
+        var DefaultRoute = Router.DefaultRoute;
+        
+        export var Component = React.jsx(`
+            <Route handler={App} name="home" path="/">
+                <Route name="info" handler={Info} />
+                <DefaultRoute handler={List} />
+            </Route>
+        `);
+    }
+    
+    var flux = new Fluxxor.Flux(Stores, Actions);
+    
+    flux.on("dispatch", (type: string, payload: any) => {
+        if (console && console.log) {
+            console.log("[Dispatch]", type, payload);
+        }
+    });
+    
+    Router.run(Routes.Component, (RouteHandler: any, state: any) => {
+        React.render(React.jsx(`<RouteHandler flux={flux} />`), document.getElementById("app"));
+    });
 }
--- typescript_react/src/app.ts
+++ typescript_react/src/app.ts
@@ -3,32 +3,32 @@
 import React = require('react/addons');
 
 class AppState {
-  data: number[] = [];
+    data: number[] = [];
 }
 
 class App extends React.Component<any, AppState, any> {
-  state = new AppState;
-  
-  render() {
-    return React.jsx(`
-      <div>
-        <button onClick={this.onClick.bind(this)}>add</button>
-        <button onClick={this.onClear.bind(this)}>clear</button>
-        <ul>{this.state.data.map((v) =>
-          <li key={v}>{v}</li>)
-        }</ul>
-      </div>
-    `);
-  }
-  
-  onClear(e: React.SyntheticEvent) {
-    this.setState({data: []});
-  }
-  
-  onClick(e: React.SyntheticEvent) {
-    this.state.data.push(this.state.data.length + 1);
-    this.setState({data: this.state.data});
-  }
+    state = new AppState;
+    
+    render() {
+        return React.jsx(`
+            <div>
+                <button onClick={this.onClick.bind(this)}>add</button>
+                <button onClick={this.onClear.bind(this)}>clear</button>
+                <ul>{this.state.data.map((v) =>
+                    <li key={v}>{v}</li>)
+                }</ul>
+            </div>
+        `);
+    }
+    
+    onClear(e: React.SyntheticEvent) {
+        this.setState({data: []});
+    }
+    
+    onClick(e: React.SyntheticEvent) {
+        this.state.data.push(this.state.data.length + 1);
+        this.setState({data: this.state.data});
+    }
 }
 
 export = App;
