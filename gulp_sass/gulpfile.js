3395d3b66234ebca0e31e9456e84730560f7ef9f
Yuichi Murata <mrk21info+github@gmail.com>
2015-02-18 18:04:21 +0900

    Intruduced the compass package to the gulp_sass.

--- gulp_sass/gulpfile.js
+++ gulp_sass/gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var compass = require('gulp-compass');
+var path = require('path');
 
 gulp.task('default', function(){
   return gulp.src('test.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./build'));
+    .pipe(compass({
+      project: __dirname,
+      bundle_exec: true,
+      sourcemap: true,
+      css: path.join(__dirname, 'build'),
+      sass: __dirname
+    }));
 });
