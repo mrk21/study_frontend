15c1f579d107c0de09aeea97026e889ae0138fdb
Yuichi Murata <mrk21info+github@gmail.com>
2015-02-19 21:50:17 +0900

    Updated the react_testing.

--- react_testing/gulpfile.js
+++ react_testing/gulpfile.js
@@ -1,5 +1,36 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var exec = require('gulp-exec');
+
+var jsx = function(){
+  var jsxLoader = require('ts-jsx-loader');
+  var gutil = require('gulp-util');
+  var through = require('through2');
+  
+  var context = {
+    query: '',
+    cacheable: function(){},
+    emitError: function(e){ throw e; }
+  };
+  
+  function transform(file, encoding, callback){
+    var result = jsxLoader.call(context, file.contents.toString('utf8'));
+    var output = new gutil.File({
+      cwd: file.cwd,
+      base: file.base,
+      path: file.path
+    });
+    output.contents = new Buffer(result);
+    this.push(output);
+    return callback();
+  }
+  
+  function flush(callback){
+    return callback();
+  }
+  
+  return through.obj(transform, flush);
+};
 
 gulp.task('default', function(){
   return gulp.src('src/index.js')
@@ -7,18 +38,24 @@ gulp.task('default', function(){
       output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
+      },
+      module: {
+        loaders: [
+          {test: /\.js$/, loader: 'ts-jsx'}
+        ]
       }
     }))
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('test', function(){
-  return gulp.src('src/test.js')
-    .pipe(webpack({
-      output: {
-        path: __dirname + '/build',
-        filename: 'bundle.js'
-      }
-    }))
+gulp.task('build_test', function(){
+  return gulp.src('src/**/*.js')
+    .pipe(jsx())
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('test', ['build_test'], function(){
+  return gulp.src('build/test.js')
+    .pipe(exec('node <%= file.path %>'))
+    .pipe(exec.reporter());
 });
--- react_testing/src/component.js
+++ react_testing/src/component.js
@@ -2,9 +2,11 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function(){
-    return React.DOM.div({}, [
-      React.DOM.a({onClick: this.onClick, ref: 'link', key: 1}, 'to home')
-    ]);
+    return React.jsx(`
+      <div>
+        <a onClick={this.onClick} ref="link" key="1" href="">to home</a>
+      </div>
+    `);
   },
   onClick: function(e) {
     console.log(1);
