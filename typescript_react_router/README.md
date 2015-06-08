0a19abbf989901e538f74ea76ef098fbca3d3d26
Yuichi Murata <mrk21info+github@gmail.com>
2015-06-08 18:52:01 +0900

    Added react-router 0.13.x test code to the typescript_react_router.

--- typescript_react_router/README.md
+++ typescript_react_router/README.md
@@ -8,8 +8,8 @@
 ## Running this project
 
 1. Run `npm install` command
-1. Run `./dtsm install` command
-1. Run `./glup` command
+1. Run `./node_modules/.bin/dtsm install` command
+1. Run `./node_modules/.bin/glup` command
 1. Open `index.html` file by your browser
 
 ### gulp tasks
--- typescript_react_router/src/index.ts
+++ typescript_react_router/src/index.ts
@@ -4,4 +4,80 @@
 import React = require('react/addons');
 import Router = require('react-router');
 
-console.log(1);
+import Route = Router.Route;
+import DefaultRoute = Router.DefaultRoute;
+import RouteHandler = Router.RouteHandler;
+import Link = Router.Link;
+
+
+class App extends React.Component<any, any> {
+    render() {
+        return React.jsx(`
+            <div>
+                <h1><Link to="home">www</Link></h1>
+                <ul>
+                    <li><Link to="info">info</Link></li>
+                </ul>
+                <RouteHandler />
+            </div>
+        `);
+    }
+}
+
+
+class ListState {
+    data: number[] = [];
+}
+class List extends React.Component<any, ListState> {
+    state = new ListState;
+    
+    render() {
+        return React.jsx(`
+            <div>
+                <button onClick={this.onClick.bind(this)}>add</button>
+                <button onClick={this.onClear.bind(this)}>clear</button>
+                <ul>{this.state.data.map((v) =>
+                    <li key={v}>{v}</li>
+                )}</ul>
+            </div>
+        `);
+    }
+    
+    onClear(e: React.SyntheticEvent) {
+        this.setState({data: []});
+    }
+    
+    onClick(e: React.SyntheticEvent) {
+        this.state.data.push(this.state.data.length + 1);
+        this.setState({data: this.state.data});
+    }
+}
+
+
+class Info extends React.Component<any, any> {
+    static contextTypes = {
+        router: React.PropTypes.func
+    }
+    
+    render() {
+        return React.jsx(`<p onClick={this.onClick.bind(this)}>info</p>`);
+    }
+    
+    onClick() {
+        var router: Router.Context = this.context.router;
+        console.log(router.getCurrentPath());
+        console.log(router.isActive('info'));
+        router.transitionTo('home');
+    }
+}
+
+
+var routes = React.jsx(`
+    <Route handler={App} name="home" path="/">
+        <Route name="info" handler={Info} />
+        <DefaultRoute handler={List} />
+    </Route>
+`);
+Router.run(routes, (Handler, state) => {
+    React.render(React.jsx(`<Handler />`), document.getElementById("app"));
+});
