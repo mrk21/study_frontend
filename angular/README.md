edd247bd66aeb7fae9538f77a8c2d531d6def078
Yuichi Murata <mrk21info+github@gmail.com>
2015-08-18 22:48:34 +0900

    Separated the todo data to the factory.

--- angular/README.md
+++ angular/README.md
@@ -10,3 +10,5 @@
 
 * [初心者のためのAngularJSの使い方入門 | Designup](http://designup.jp/angularjs-getstarted-337/)
 * [AngularJS 入門 - Qiita](http://qiita.com/lga0503/items/d8efddcad2574e1938f1#2-4)
+* [AngularJS ではモデルをどう宣言すればいいのか - AngularJS Ninja Blog](http://angularjsninja.com/blog/2013/08/28/how-to-declare-models/)
+* [AngularJS 1.4 と Browserify を組み合わせて試してみる | スマホ神 – JavaScript 受託開発 –](http://smart.ataglance.jp/2015-03-26-try-angularjs-1-4-with-browserify/)
--- angular/src/index.js
+++ angular/src/index.js
@@ -2,12 +2,16 @@ var angular = require('angular');
 
 var TodoApp = angular.module('TodoApp', []);
 
-TodoApp.controller('TodoCtrl', function ($scope) {
-  $scope.todos = [];
+TodoApp.factory('Todo', function(){
+  return [];
+});
+
+TodoApp.controller('TodoCtrl', function($scope, Todo){
+  $scope.todos = Todo;
   index = 0;
   $scope.add = function() {
     if (!$scope.message) return;
-    $scope.todos.push({id: index, message: $scope.message, done: false});
+    Todo.push({id: index, message: $scope.message, done: false});
     $scope.message = "";
     index++;
   };
