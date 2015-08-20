0c194166f1b0ab3fcb2e94a31f4182543c54eb80
Yuichi Murata <mrk21info+github@gmail.com>
2015-08-20 23:17:16 +0900

    Separated the todo addition form to the directive in the angular.

--- angular/README.md
+++ angular/README.md
@@ -12,3 +12,4 @@
 * [AngularJS 入門 - Qiita](http://qiita.com/lga0503/items/d8efddcad2574e1938f1#2-4)
 * [AngularJS ではモデルをどう宣言すればいいのか - AngularJS Ninja Blog](http://angularjsninja.com/blog/2013/08/28/how-to-declare-models/)
 * [AngularJS 1.4 と Browserify を組み合わせて試してみる | スマホ神 – JavaScript 受託開発 –](http://smart.ataglance.jp/2015-03-26-try-angularjs-1-4-with-browserify/)
+* [JavaScript - AngularJSのDirectiveを理解する. - Qiita](http://qiita.com/Quramy/items/dd4e7d2693c32d92048c)
--- angular/src/index.js
+++ angular/src/index.js
@@ -6,13 +6,25 @@ TodoApp.factory('Todo', function(){
   return [];
 });
 
+
+// Aungular's directive is like Web Components.
+TodoApp.directive('todoForm', function(){
+  return {
+    templateUrl: 'src/todo_form.html',
+    
+    controller: function($scope, Todo){
+      index = 0;
+      
+      $scope.add = function(){
+        if (!$scope.message) return;
+        Todo.push({id: index, message: $scope.message, done: false});
+        $scope.message = "";
+        index++;
+      };
+    }
+  };
+});
+
 TodoApp.controller('TodoCtrl', function($scope, Todo){
   $scope.todos = Todo;
-  index = 0;
-  $scope.add = function() {
-    if (!$scope.message) return;
-    Todo.push({id: index, message: $scope.message, done: false});
-    $scope.message = "";
-    index++;
-  };
 });
