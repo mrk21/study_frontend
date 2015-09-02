ff6ba735d4e1c5b7a8b5476550ef44ee04a185df
Yuichi Murata <mrk21info+github@gmail.com>
2015-09-02 23:45:51 +0900

    Added the todoItem directive to the angular.

---
+++ angular/app/directives/todo_item.js
@@ -0,0 +1,17 @@
+var angular = require('angular');
+var app = require('app');
+
+module.exports = app.directive('todoItem', function(){
+  return {
+    replace: false,
+    templateUrl: 'app/views/todo_item.html',
+    
+    scope: {
+      todo: '@'
+    },
+    
+    link: function(scope, element, attrs){
+      scope.todo = scope.$eval(attrs.todoItem);
+    }
+  };
+});
