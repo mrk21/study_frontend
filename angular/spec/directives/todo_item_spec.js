4063decae0940a569991fb8ac3505b2ac0264203
Yuichi Murata <mrk21info+github@gmail.com>
2015-09-13 22:27:25 +0900

    angular: Fix directive event listenar spec

--- angular/spec/directives/todo_item_spec.js
+++ angular/spec/directives/todo_item_spec.js
@@ -1,67 +1,50 @@
 describe('directives.todoItem', function() {
-  var $compile, $rootScope, template, Todo, $q;
+  var $compile, $rootScope, template, Todo, $httpBackend;
   
   beforeEach(module('TodoApp'));
   
-  beforeEach(inject(function($templateCache, _$compile_, _$rootScope_, _Todo_, _$q_) {
+  beforeEach(inject(function($templateCache, _$compile_, _$rootScope_, _Todo_, _$httpBackend_) {
     template = window.__html__['app/views/todo_item.html'];
     $templateCache.put('app/views/todo_item.html', template);
     $compile = _$compile_;
     $rootScope = _$rootScope_;
     Todo = _Todo_;
-    $q = _$q_;
+    $httpBackend = _$httpBackend_;
   }));
   
   describe('when the remove button was clicked', function(){
-    var deferred;
-    
-    beforeEach(function(){
-      deferred = $q.defer();
-      setTimeout(function(){
-        deferred.resolve({});
-      }, 0);
-    });
-    
-    it('should call `Todo#remove()` and `$scope.onRemove()`', function(done){
+    it('should call `$scope.onRemove()`', function(){
       $rootScope.onRemove = function(){};
       $rootScope.todo = new Todo({id: 1, content: 'a', done: false});
       
+      $httpBackend.expectDELETE('/api/todos/1').respond(200, JSON.stringify($rootScope.todo));
       spyOn($rootScope, 'onRemove');
-      spyOn($rootScope.todo, 'remove').and.callFake(function(){
-        return deferred.promise;
-      });
       
       var element = angular.element('<div todo-item="todo" todo-item-removed="onRemove"></div>');
       $compile(element)($rootScope);
       $rootScope.$digest();
       
-      setTimeout(function(){
-        element.find('button')[0].click();
-        expect($rootScope.todo.remove).toHaveBeenCalled();
-        expect($rootScope.onRemove).toHaveBeenCalled();
-        done();
-      }, 0);
+      element.find('button')[0].click();
+      $httpBackend.flush();
+      
+      expect($rootScope.onRemove).toHaveBeenCalled();
     });
     
-    it('should call `Todo#save()` and `$scope.onUpdate()`', function(done){
+    it('should call `$scope.onUpdate()`', function(){
       $rootScope.onUpdate = function(){};
       $rootScope.todo = new Todo({id: 1, content: 'a', done: false});
       
+      $httpBackend.expectPUT('/api/todos/1').respond(200, JSON.stringify($rootScope.todo));
       spyOn($rootScope, 'onUpdate');
-      spyOn($rootScope.todo, 'save').and.callFake(function(){
-        return deferred.promise;
-      });
       
       var element = angular.element('<div todo-item="todo" todo-item-updated="onUpdate"></div>');
       $compile(element)($rootScope);
       $rootScope.$digest();
       
-      setTimeout(function(){
-        element.find('input')[0].click();
-        expect($rootScope.todo.save).toHaveBeenCalled();
-        expect($rootScope.onUpdate).toHaveBeenCalled();
-        done();
-      }, 0);
+      element.find('input')[0].click();
+      $httpBackend.flush();
+      
+      expect($rootScope.onUpdate).toHaveBeenCalled();
     });
   });
 });
