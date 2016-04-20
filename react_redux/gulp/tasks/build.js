5f930a1f83beb726579547872a89e6616296543b
Yuichi Murata <mrk21info+github@gmail.com>
2016-04-20 22:41:13 +0900

    react_redux: introduce postcss

--- react_redux/gulp/tasks/build.js
+++ react_redux/gulp/tasks/build.js
@@ -1,8 +1,10 @@
 import gulp from 'gulp';
 import webpack from 'webpack';
 import webpackConfig from 'config/webpack.config';
+import postcss from 'gulp-postcss'
+import autoprefixer from 'autoprefixer';
 
-gulp.task('build', ['build:js','build:html']);
+gulp.task('build', ['build:js','build:css','build:html']);
 
 gulp.task('build:js', done => {
   webpack(webpackConfig, (error, stats) => {
@@ -13,5 +15,13 @@ gulp.task('build:js', done => {
 
 gulp.task('build:html', () =>
   gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('build:css', () =>
+  gulp.src('src/**/*.css')
+    .pipe(postcss([
+      autoprefixer({browsers: ['last 1 versions', 'ie >= 10']})
+    ]))
     .pipe(gulp.dest('dist'))
 );
