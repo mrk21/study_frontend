60baa854fd7ba1f3e8aeac29b3a3b4284f09b0d6
Yuichi Murata <mrk21info+github@gmail.com>
2016-06-02 23:59:29 +0900

    redux-saga: add a code of fork and join

--- redux-saga/src/sagas/non_blocking_calls.js
+++ redux-saga/src/sagas/non_blocking_calls.js
@@ -1,11 +1,24 @@
-import { call } from 'redux-saga/effects';
+import { delay } from 'redux-saga';
+import { fork, join, call } from 'redux-saga/effects';
 
 function func() {
   console.log(1.5);
+}
+
+function* generator(i) {
+  yield call(delay, 1000);
+  console.log(yield i);
 }
 
 export default function* api() {
   console.log(yield 1);
   yield call(func);
   console.log(yield 2);
+  
+  yield fork(generator, 3);
+  console.log(yield 4);
+  
+  const task = yield fork(generator, 5);
+  yield join(task);
+  console.log(yield 6);
 }
