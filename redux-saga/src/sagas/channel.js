62d376e24ac888c3f9c1a0e02fa3c12a0022aab3
Yuichi Murata <mrk21info+github@gmail.com>
2016-06-09 23:59:29 +0900

    redux-saga: add a code of action channel

--- redux-saga/src/sagas/channel.js
+++ redux-saga/src/sagas/channel.js
@@ -1,10 +1,11 @@
+import { buffers } from 'redux-saga';
 import { take, put, actionChannel } from 'redux-saga/effects';
 
 export default function* channel() {
-  const chan = yield actionChannel('FETCH');
-  console.log('## channel', chan);
+  const chan = yield actionChannel('FETCH', buffers.sliding(5));
+  console.log('channel', chan);
   while (true) {
     const action = yield take(chan);
-    console.log('## channel', action);
+    console.log('channel', action);
   }
 }
