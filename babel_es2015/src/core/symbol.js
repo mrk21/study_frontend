ee594c1be66bfed5fe5538701d34c6965a6c92f7
Yuichi Murata <mrk21info+github@gmail.com>
2016-06-12 23:59:22 +0900

    babel_es2015: add a code of symbol

---
+++ babel_es2015/src/core/symbol.js
@@ -0,0 +1,19 @@
+{
+  console.log(`Symbol('test') === Symbol('test') =>`, Symbol('test') === Symbol('test'));
+}
+{
+  const privateKey = Symbol('c');
+  console.log(`privateKey = Symbol('c')`);
+  const obj = {
+    a: 1,
+    b: 2,
+    [privateKey]: 3
+  };
+  console.log('obj = {a: 1, b: 2, [privateKey]: 3} =>', obj);
+  console.log(`obj['c'] =>`, obj['c']);
+  console.log(`obj[privateKey] =>`, obj[privateKey]);
+  console.log(`obj[Symbol('c')] =>`, obj[Symbol('c')]);
+  for (const key in obj) {
+    console.log('for (const key in obj)', key);
+  }
+}
--- babel_es2015/src/index.js
+++ babel_es2015/src/index.js
@@ -1,1 +1,2 @@
 import iterator from './core/iterator';
+import symbol  from './core/symbol';
