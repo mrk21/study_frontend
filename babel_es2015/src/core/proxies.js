3cf5a3b41d406531318b188f1b5e9a43f3bdb973
Yuichi Murata <mrk21info+github@gmail.com>
2016-06-16 17:01:02 +0900

    babel_es2015: add a code of proxy(method missing)

--- babel_es2015/src/core/proxies.js
+++ babel_es2015/src/core/proxies.js
@@ -1,4 +1,5 @@
 console.group('proxies')
+console.group('basic')
 {
   const target = {
     value1: 1
@@ -14,4 +15,31 @@ console.group('proxies')
   console.log(proxy.value1); // 1
   console.log(proxy.value2); // not found
 }
+console.groupEnd();
+
+console.group('method missing')
+{
+  const target = {
+    method1(value) {
+      return value;
+    },
+    methodMissing(name) {
+      return (...args) => {
+        console.log(`${name}() is not found`);
+        return args;
+      };
+    }
+  };
+  const handler = {
+    get(receiver, name) {
+      console.log(receiver, name);
+      if (name in receiver) return receiver[name];
+      return receiver.methodMissing(name);
+    }
+  };
+  const proxy = new Proxy(target, handler);
+  console.log(proxy.method1(1)); // 1
+  console.log(proxy.method2(1,2,3)); // [1,2,3]
+}
+console.groupEnd();
 console.groupEnd();
