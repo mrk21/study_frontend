97add6c94227933f96fe3af01b8c015f8e44d92d
Yuichi Murata <mrk21info+github@gmail.com>
2016-06-26 23:53:55 +0900

    babel_es2015: add a code of reflect api

--- babel_es2015/src/core/reflect.js
+++ babel_es2015/src/core/reflect.js
@@ -1,12 +1,15 @@
 console.group('reflect')
 {
   const target = {
-    value1: 1
+    value1: 1,
+    value2: 2,
+    func1() {},
   };
   console.log('target =>', target)
   console.log("Reflect.has(target, 'value1') =>", Reflect.has(target, 'value1'));
   console.log("Reflect.get(target, 'value1') =>", Reflect.get(target, 'value1'));
   console.log("Reflect.set(target, 'value1', 2) =>", Reflect.set(target, 'value1', 2));
   console.log("Reflect.deleteProperty(target, 'value1') =>", Reflect.deleteProperty(target, 'value1'));
+  console.log("Reflect.ownKeys(target) =>", Reflect.ownKeys(target));
 }
 console.groupEnd();
