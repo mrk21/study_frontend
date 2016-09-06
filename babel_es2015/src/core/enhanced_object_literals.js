dda57a0c551865b5d489e6528a6ee7e016865d7d
Yuichi Murata <mrk21info+github@gmail.com>
2016-09-06 23:52:44 +0900

    babel_es2015: fix eslint errors

--- babel_es2015/src/core/enhanced_object_literals.js
+++ babel_es2015/src/core/enhanced_object_literals.js
@@ -3,30 +3,30 @@ console.group('enhanced_object_literals');
   const proto = {
     protoMethod() {
       console.log('protoMethod');
-    }
+    },
   };
-  
+
   const handler = () => {
     console.log('handler');
   };
-  
+
   const obj = {
     // __proto__
     __proto__: proto,
-    
+
     // Shorthand for `handler: handler`
     handler,
-    
+
     // Methods
     toString() {
-     // Super calls
-     return "my toString() - " + super.toString();
+      // Super calls
+      return `my toString() - ${super.toString()}`;
     },
-    
+
     // Computed (dynamic) property names
-    [ 'prop_' + (() => 42)() ]: 42
+    [`prop_${2 * 3}`]: 42,
   };
-  
+
   obj.protoMethod();
   obj.handler();
   obj.toString();
