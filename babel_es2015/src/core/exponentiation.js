54a8022766f234f57e3088798a6ea9f684488316
Yuichi Murata <mrk21info+github@gmail.com>
2016-09-08 23:26:39 +0900

    babel_es2015: fix eslint errors

--- babel_es2015/src/core/exponentiation.js
+++ babel_es2015/src/core/exponentiation.js
@@ -1,5 +1,5 @@
 console.group('ES2016: Exponentiation');
 {
-  console.log(`2 ** 3`, 2 ** 3);
+  console.log('2 ** 3', 2 ** 3);
 }
 console.groupEnd();
--- babel_es2015/src/core/iterators.js
+++ babel_es2015/src/core/iterators.js
@@ -1,8 +1,8 @@
 console.group('iterators');
 {
   const iterable = {
-    values: [1,2,3],
-    
+    values: [1, 2, 3],
+
     [Symbol.iterator]() {
       let i = 0;
       return {
@@ -10,12 +10,12 @@ console.group('iterators');
           const value = this.values[i++];
           const done = typeof value === 'undefined';
           return { value, done };
-        }
+        },
       };
-    }
+    },
   };
-  
-  for (let value of iterable) {
+
+  for (const value of iterable) {
     console.log(value);
   }
 }
