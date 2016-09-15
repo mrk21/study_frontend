fabbab79956e584e5351cc8d947041bda00666bb
Yuichi Murata <mrk21info+github@gmail.com>
2016-09-15 23:59:47 +0900

    babel_es2015: fix eslint errors

--- babel_es2015/src/library/map.js
+++ babel_es2015/src/library/map.js
@@ -1,17 +1,17 @@
 console.group('map');
 {
-  let map = new Map();
-  
+  const map = new Map();
+
   const stringKey = 'string';
-  const objectKey = {a: 'object'};
+  const objectKey = { a: 'object' };
   const funcKey = () => 'function';
-  
+
   map.set(stringKey, 'string'); console.log('string key =>', stringKey);
   map.set(objectKey, 'object'); console.log('object key =>', objectKey);
   map.set(funcKey, 'function'); console.log('func key =>', funcKey);
-  
+
   console.log('map.size =>', map.size);
-  
+
   console.log('map.get(stringKey) =>', map.get(stringKey));
   console.log('map.get(objectKey) =>', map.get(objectKey));
   console.log('map.get(funcKey) =>', map.get(funcKey));
