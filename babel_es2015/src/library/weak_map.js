856810c7d918f01ad435059fa4eedf2113da1892
Yuichi Murata <mrk21info+github@gmail.com>
2016-09-16 23:59:07 +0900

    babel_es2015: fix eslint errors

--- babel_es2015/src/library/weak_map.js
+++ babel_es2015/src/library/weak_map.js
@@ -1,31 +1,31 @@
 console.group('weak map');
 {
-  let weakMap = new WeakMap();
-  
-  let stringKey = new String('string');
-  let objectKey = {a: 'object'};
+  const weakMap = new WeakMap();
+
+  let stringKey = String('string');
+  let objectKey = { a: 'object' };
   let funcKey = () => 'function';
-  
+
   let stringValue = 'string';
-  let objectValue = {a: 'object'};
+  let objectValue = { a: 'object' };
   let funcValue = () => 'function';
-  
+
   console.log('stringKey =>', stringKey);
   console.log('objectKey =>', objectKey);
   console.log('funcKey =>', funcKey);
-  
+
   console.log('stringValue =>', stringValue);
   console.log('objectValue =>', objectValue);
   console.log('funcValue =>', funcValue);
-  
+
   weakMap.set(stringKey, stringValue);
   weakMap.set(objectKey, objectValue);
   weakMap.set(funcKey, funcValue);
-  
+
   console.log('weakMap.has(stringKey) =>', weakMap.has(stringKey));
   console.log('weakMap.has(objectKey) =>', weakMap.has(objectKey));
   console.log('weakMap.has(funcKey) =>', weakMap.has(funcKey));
-  
+
   console.group('stringKey');
   stringValue = null;
   console.log('stringValue = null');
@@ -34,7 +34,7 @@ console.group('weak map');
   console.log('stringKey = null');
   console.log('weakMap.has(stringKey) =>', weakMap.has(stringKey));
   console.groupEnd();
-  
+
   console.group('objectKey');
   objectValue = null;
   console.log('objectValue = null');
@@ -43,7 +43,7 @@ console.group('weak map');
   console.log('objectKey = null');
   console.log('weakMap.has(objectKey) =>', weakMap.has(objectKey));
   console.groupEnd();
-  
+
   console.group('funcKey');
   funcValue = null;
   console.log('funcValue = null');
--- babel_es2015/src/library/weak_set.js
+++ babel_es2015/src/library/weak_set.js
@@ -1,14 +1,14 @@
 console.group('weak set');
 {
-  let weakSet = new WeakSet();
-  let value = {a: 1};
-  
+  const weakSet = new WeakSet();
+  let value = { a: 1 };
+
   console.log('value => ', value);
   console.log('weakSet.add(value)');
   weakSet.add(value);
   console.log('has(value) =>', weakSet.has(value));
   console.log('[...weakSet]', [...weakSet]); // not iterable
-  
+
   value = null;
   console.log('value =>', value);
   console.log('has(value) =>', weakSet.has(value));
