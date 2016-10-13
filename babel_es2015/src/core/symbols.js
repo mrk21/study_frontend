86a848b593bed65ec1ee345d45f3c69e9933a665
Yuichi Murata <mrk21info+github@gmail.com>
2016-10-13 23:57:54 +0900

    babel_es2015: improve code execution method

--- babel_es2015/src/core/symbols.js
+++ babel_es2015/src/core/symbols.js
@@ -2,24 +2,26 @@
 /* eslint guard-for-in: 0 */
 /* eslint no-restricted-syntax: 0 */
 
-console.group('symbols');
-{
-  console.log("Symbol('test') === Symbol('test') =>", Symbol('test') === Symbol('test'));
-}
-{
-  const privateKey = Symbol('c');
-  console.log("privateKey = Symbol('c')");
-  const obj = {
-    a: 1,
-    b: 2,
-    [privateKey]: 3,
-  };
-  console.log('obj = {a: 1, b: 2, [privateKey]: 3} =>', obj);
-  console.log("obj['c'] =>", obj['c']);
-  console.log('obj[privateKey] =>', obj[privateKey]);
-  console.log("obj[Symbol('c')] =>", obj[Symbol('c')]);
-  for (const key in obj) {
-    console.log('for (const key in obj)', key);
+export default function symbols() {
+  console.group('symbols');
+  {
+    console.log("Symbol('test') === Symbol('test') =>", Symbol('test') === Symbol('test'));
   }
+  {
+    const privateKey = Symbol('c');
+    console.log("privateKey = Symbol('c')");
+    const obj = {
+      a: 1,
+      b: 2,
+      [privateKey]: 3,
+    };
+    console.log('obj = {a: 1, b: 2, [privateKey]: 3} =>', obj);
+    console.log("obj['c'] =>", obj['c']);
+    console.log('obj[privateKey] =>', obj[privateKey]);
+    console.log("obj[Symbol('c')] =>", obj[Symbol('c')]);
+    for (const key in obj) {
+      console.log('for (const key in obj)', key);
+    }
+  }
+  console.groupEnd();
 }
-console.groupEnd();
--- babel_es2015/src/index.js
+++ babel_es2015/src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import classSyntax from './core/class_syntax';
 import iterators from './core/iterators';
 import generator from './core/generator';
-import './core/symbols';
+import symbols from './core/symbols';
 import './core/enhanced_object_literals';
 import destructuringAssignment from './core/destructuring_assignment';
 import asyncFunctions from './core/async_functions';
@@ -21,6 +21,7 @@ async function main() {
   await classSyntax();
   await iterators();
   await generator();
+  await symbols();
   await destructuringAssignment();
   await asyncFunctions();
 }
