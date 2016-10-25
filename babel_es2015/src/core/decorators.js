c329a782f7a2dbf213e28a7d55669d930b365cf8
Yuichi Murata <mrk21info+github@gmail.com>
2016-10-25 23:59:07 +0900

    babel_es2015: fix a code of decorators

--- babel_es2015/src/core/decorators.js
+++ babel_es2015/src/core/decorators.js
@@ -1,7 +1,29 @@
 export default function decorators() {
   console.group('ES-next: Stage-2: decorators');
   {
-    console.log(`
+    console.group('Base');
+      console.log(`
+        class Hoge {
+          @decorator
+          instance = 1;
+
+          @decorator()
+          method() {
+            console.log('Hoge.prototype.method()');
+          }
+        }
+
+        function decorator(...args) {
+          console.log('decorator()', ...args);
+          return function(target, key, descriptor) {
+            console.log('decorator(): target =>', target);
+            console.log('decorator(): key =>', key);
+            console.log('decorator(): descriptor =>', descriptor);
+            return descriptor;
+          };
+        }
+      `);
+
       class Hoge {
         @decorator
         instance = 1;
@@ -14,37 +36,18 @@ export default function decorators() {
 
       function decorator(...args) {
         console.log('decorator()', ...args);
-        return function(target, key, descriptor) {
+        return (target, key, descriptor) => {
           console.log('decorator(): target =>', target);
           console.log('decorator(): key =>', key);
           console.log('decorator(): descriptor =>', descriptor);
           return descriptor;
         };
       }
-    `);
 
-    class Hoge {
-      @decorator
-      instance = 1;
-
-      @decorator()
-      method() {
-        console.log('Hoge.prototype.method()');
-      }
+      const hoge = new Hoge();
+      hoge.method();
     }
-
-    function decorator(...args) {
-      console.log('decorator()', ...args);
-      return (target, key, descriptor) => {
-        console.log('decorator(): target =>', target);
-        console.log('decorator(): key =>', key);
-        console.log('decorator(): descriptor =>', descriptor);
-        return descriptor;
-      };
-    }
-
-    const hoge = new Hoge();
-    hoge.method();
+    console.groupEnd();
   }
   console.groupEnd();
 }
