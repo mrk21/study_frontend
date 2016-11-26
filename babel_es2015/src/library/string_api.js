0616de35e72a41b7df33d2c074cda08da03c4885
Yuichi Murata <mrk21info+github@gmail.com>
2016-11-26 23:39:01 +0900

    babel_es2015: add a code of String.prototype.{trimLeft,trimRight}(Stage-2)

--- babel_es2015/src/library/string_api.js
+++ babel_es2015/src/library/string_api.js
@@ -2,24 +2,36 @@
 
 export default function stringApi() {
   console.group('String API');
-  console.group('new API');
   {
-    console.log("'abcd'.includes('bc') =>", 'abcd'.includes('bc'));
-    console.log("'abc'.repeat(3) =>", 'abc'.repeat(3));
-  }
-  console.groupEnd();
+    console.group('new API');
+    {
+      console.log("'abcd'.includes('bc') =>", 'abcd'.includes('bc'));
+      console.log("'abc'.repeat(3) =>", 'abc'.repeat(3));
+    }
+    console.groupEnd();
 
-  console.group('unicode');
-  {
-    console.log("'𠮷'.length =>", '𠮷'.length);
-    console.log("'𠮷'.match(/./u)[0].length =>", '𠮷'.match(/./u)[0].length);
-    console.log("'\\u{20BB7}' === '𠮷'' =>", '\u{20BB7}' === '𠮷');
-    console.log("'𠮷' === '\\uD842\\uDFB7' =>", '𠮷' === '\uD842\uDFB7');
-    console.log("'𠮷'.codePointAt(0) =>", '𠮷'.codePointAt(0));
-    console.group('for(const c of "𠮷") console.log(c)');
-    for (const c of '𠮷') console.log(c);
+    console.group('unicode');
+    {
+      console.log("'𠮷'.length =>", '𠮷'.length);
+      console.log("'𠮷'.match(/./u)[0].length =>", '𠮷'.match(/./u)[0].length);
+      console.log("'\\u{20BB7}' === '𠮷'' =>", '\u{20BB7}' === '𠮷');
+      console.log("'𠮷' === '\\uD842\\uDFB7' =>", '𠮷' === '\uD842\uDFB7');
+      console.log("'𠮷'.codePointAt(0) =>", '𠮷'.codePointAt(0));
+
+      console.group('for(const c of "𠮷") console.log(c)');
+      {
+        for (const c of '𠮷') console.log(c);
+      }
+      console.groupEnd();
+    }
+    console.groupEnd();
+
+    console.group('ES-Next: Stage-2: String.prototype.{trimLeft,trimRight}');
+    {
+      console.log("'  hoge  '.trimLeft() =>", `'${'  hoge  '.trimLeft()}'`);
+      console.log("'  hoge  '.trimRight() =>", `'${'  hoge  '.trimRight()}'`);
+    }
     console.groupEnd();
   }
-  console.groupEnd();
   console.groupEnd();
 }
