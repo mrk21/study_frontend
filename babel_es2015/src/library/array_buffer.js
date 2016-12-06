4af346e59644806588ae495095fbbe468cb8ec4d
Yuichi Murata <mrk21info+github@gmail.com>
2016-12-06 23:11:29 +0900

    babel_es2015: add a code of ArrayBuffer/ArrayBufferView

--- babel_es2015/src/library/array_buffer.js
+++ babel_es2015/src/library/array_buffer.js
@@ -2,7 +2,10 @@ export default function arrayBuffer() {
   console.group('ArrayBuffer/ArrayBufferView');
   {
     const buffer = new ArrayBuffer(8); // 8 bytes
-    const view = new Int32Array(buffer);
+    const view = new Uint8Array(buffer);
+    view[0] = 0xFF;
+    view[1] = 0x0F;
+    view[2] = 0x0A;
     console.log(view);
   }
   console.groupEnd();
