5fc89ceb1b36c555a5954ee69f661a32dc3e0565
Yuichi Murata <mrk21info+github@gmail.com>
2016-12-14 21:51:20 +0900

    babel_es2015: add a code of ArrayBuffer/ArrayBufferView

--- babel_es2015/src/library/array_buffer.js
+++ babel_es2015/src/library/array_buffer.js
@@ -1,52 +1,75 @@
 export default function arrayBuffer() {
-  console.group('ArrayBuffer/ArrayBufferView');
+  console.group('ArrayBuffer/TypedArray/DataView');
   {
-    console.group('base');
+    console.group('ArrayBuffer');
     {
-      const buffer = new ArrayBuffer(8); // 8 bytes
-      const view = new Uint8Array(buffer);
-      view[0] = 0xFF;
-      view[1] = 0x0F;
-      view[2] = 0x0A;
-      console.log(view);
-    }
-    console.groupEnd();
-
-    console.group('range error');
-    {
-      try {
-        const buffer = new ArrayBuffer(7); // 7 bytes
-        const view = new Uint16Array(buffer);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    console.groupEnd();
-
-    console.group('buffer sharing');
-    {
-      const buffer = new ArrayBuffer(2); // 2 bytes
+      console.group('buffer sharing');
       {
+        const buffer = new ArrayBuffer(2); // 2 bytes
+        {
+          const view = new Uint8Array(buffer);
+          view[0] = 0xFF;
+          view[1] = 0xFF;
+          console.log(view);
+        }
+        {
+          const view = new Int16Array(buffer);
+          console.log(view); // -1
+        }
+      }
+      console.groupEnd();
+    }
+    console.groupEnd();
+
+    console.group('TypedArray');
+    {
+      console.group('base');
+      {
+        const buffer = new ArrayBuffer(8); // 8 bytes
         const view = new Uint8Array(buffer);
         view[0] = 0xFF;
-        view[1] = 0xFF;
+        view[1] = 0x0F;
+        view[2] = 0x0A;
         console.log(view);
       }
+      console.groupEnd();
+
+      console.group('range error');
       {
-        const view = new Int16Array(buffer);
-        console.log(view); // -1
+        try {
+          const buffer = new ArrayBuffer(7); // 7 bytes
+          const view = new Uint16Array(buffer);
+        } catch (e) {
+          console.error(e);
+        }
       }
+      console.groupEnd();
     }
     console.groupEnd();
 
     console.group('DataView');
     {
-      const buffer = new ArrayBuffer(2); // 2 bytes
-      const view = new DataView(buffer);
-      view.setUint8(0, 0xFF);
-      view.setUint8(1, 0xFF);
-      console.log(view.getInt16(0));
+      console.group('base');
+      {
+        const buffer = new ArrayBuffer(2); // 2 bytes
+        const view = new DataView(buffer);
+        view.setUint8(0, 0xFF);
+        view.setUint8(1, 0xFF);
+        console.log(view.getInt16(0));
+      }
+      console.groupEnd();
 
+      console.group('endianness');
+      {
+        const buffer = new ArrayBuffer(2); // 2 bytes
+        const view = new DataView(buffer);
+        console.log('set 0xFF 0x00');
+        view.setUint8(0, 0xFF);
+        view.setUint8(1, 0x00);
+        console.log('litle endianness =>', view.getInt16(0, false));
+        console.log('big endianness =>', view.getInt16(0, true));
+      }
+      console.groupEnd();
     }
     console.groupEnd();
   }
